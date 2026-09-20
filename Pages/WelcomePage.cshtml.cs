1361ad5b05e7853a3dba9f8a685d0c56efa29c6f
gabri <gabri@GABBY_K_>
2026-09-20 15:58:17 -0500

    Add project files.

---
+++ Pages/WelcomePage.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RecruitmentProject.Pages
+{
+    public class WelcomePageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
